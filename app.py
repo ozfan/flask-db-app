be7bf25cb09069f096310d7c178faa5e5119777c
il5050 <ivan@brandmotor.com>
2022-10-20 17:17:09 +0800

    Putting files in

---
+++ app.py
@@ -0,0 +1,54 @@
+import os
+import psycopg2
+from flask import Flask, redirect, render_template, url_for
+from urllib.parse import urlparse
+
+
+app = Flask(__name__)
+
+
+def get_db_connection():
+    result = urlparse(os.environ['DATABASE_URL'])
+    username = result.username
+    password = result.password
+    database = result.path[1:]
+    hostname = result.hostname
+    port = result.port
+    connection = psycopg2.connect(
+        database=database,
+        user=username,
+        password=password,
+        host=hostname,
+        port=port
+    )
+
+    return connection
+
+
+@app.route('/<path:text>', methods=['GET', 'POST'])
+def all_routes(text):
+    if text.startswith('pages') or text.startswith('sections'):
+        return render_template(text)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    conn = get_db_connection()
+    # create a cursor
+    cur = conn.cursor()
+
+    # execute a statement
+    print('PostgreSQL database version:')
+    cur.execute('SELECT version()')
+
+    # display the PostgreSQL database server version
+    db_version = cur.fetchone()
+    print(db_version[0])
+
+    # close the communication with the PostgreSQL
+    cur.close()
+    return render_template('index.html', version=db_version[0])
+
+
+if __name__ == '__main__':
+    app.run()
