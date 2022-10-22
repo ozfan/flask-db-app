725c13c31cf0e5f0071593e962a31f524e2bc192
sinyou <kevinlin0638@gmail.com>
2022-10-22 17:04:33 +0800

    Version Ivan

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def hello_world():  # put application's code here
                     sum(t.kid_seat) kid_attendance FROM transaction_fact t, date_dimension d, film_dimension f 
                     WHERE t.date_key = d.date_key AND extract(month from d.date) = 9 AND t.film_key = f.film_key 
                          GROUP BY f.film_name, f.genre ORDER BY total_sales DESC;""")
-    sales_by_film = cur.fetchall()
+    session["sales_by_film"] = cur.fetchall()
     
     #Return on Investment By Film Section Display
     cur.execute("""SELECT f.film_name, f.genre, sum(t.total_amount_paid) total_sales, max(f.procurement_cost) cost, (sum(t.total_amount_paid) - max(f.procurement_cost)) profit, ((sum(t.total_amount_paid) - max(f.procurement_cost))/max(f.procurement_cost)) ROI 
@@ -63,7 +63,7 @@ def hello_world():  # put application's code here
                      WHERE t.film_key = f.film_key 
                      GROUP BY f.film_name, f.genre 
                      ORDER BY roi DESC;""")
-    film_roi = cur.fetchall()
+    session["film_roi"] = cur.fetchall()
     
     #Return on Investment By Film Section Display
     cur.execute("""SELECT p.planner, p.promotion_name, SUM(t.total_amount_paid) total_sales_from_promo, (MAX(p.cost_per_member) * count(*)) promo_costs, (SUM(t.total_amount_paid)-(MAX(p.cost_per_member) * count(*))) profit, (SUM(t.total_amount_paid)-(MAX(p.cost_per_member) * count(*)))/(MAX(p.cost_per_member) * count(*)) promo_roi 
@@ -71,7 +71,7 @@ def hello_world():  # put application's code here
                          WHERE t.date_key = d.date_key AND extract(month from d.date) = 9 AND t.promotion_key = p.promotion_key 
                          GROUP BY p.planner, p.promotion_name 
                          ORDER BY promo_roi DESC;""")
-    promo_roi = cur.fetchall()
+    session["promo_roi"] = cur.fetchall()
     
     #Most popular promotion by member’s age and gender
     cur.execute("""SELECT m1.age, m1.gender,p1.planner,p1.promotion_name, count(*) total_redemption
@@ -92,7 +92,7 @@ def hello_world():  # put application's code here
                                 AND m1.gender = m2.gender
                                 GROUP BY p2.planner, p2.promotion_name, m2.age, m2.gender)
                             ORDER BY m1.age, m1.gender DESC;""")
-    pop_promo = cur.fetchall()
+    session["pop_promo"] = cur.fetchall()
 
     # close the communication with the PostgreSQL
     cur.close()
@@ -116,18 +116,33 @@ def query_sales_dow(): #template for some query
         conn = get_db_connection()
         cur = conn.cursor()
         if Month_choice == 'September':
-            cur.execute(('SELECT d.weekday, sum(t.total_amount_paid) total_sales, sum(t.adult_amount_paid) adult_sales, sum(t.kid_amount_paid) kid_sales, sum(t.adult_seat + t.kid_seat) total_attendance, sum(t.adult_seat) adults_attendance, sum(t.kid_seat) kid_attendance FROM transaction_fact t, date_dimension d WHERE t.date_key = d.date_key and extract(month from d.date) = 9 GROUP BY d.weekday ORDER BY total_sales DESC;')) #insert query here
+            cur.execute(("""SELECT d.weekday, sum(t.total_amount_paid) total_sales, sum(t.adult_amount_paid) adult_sales, sum(t.kid_amount_paid) kid_sales, sum(t.adult_seat + t.kid_seat) total_attendance, sum(t.adult_seat) adults_attendance, sum(t.kid_seat) kid_attendance 
+            FROM transaction_fact t, date_dimension d 
+            WHERE t.date_key = d.date_key and extract(month from d.date) = 9 
+            GROUP BY d.weekday 
+            ORDER BY total_sales DESC;""")) #insert query here
         elif Month_choice == 'October':
-            cur.execute(('SELECT d.weekday, sum(t.total_amount_paid) total_sales, sum(t.adult_amount_paid) adult_sales, sum(t.kid_amount_paid) kid_sales, sum(t.adult_seat + t.kid_seat) total_attendance, sum(t.adult_seat) adults_attendance, sum(t.kid_seat) kid_attendance FROM transaction_fact t, date_dimension d WHERE t.date_key = d.date_key and extract(month from d.date) = 10 GROUP BY d.weekday ORDER BY total_sales DESC;')) #insert query here
+            cur.execute(("""SELECT d.weekday, sum(t.total_amount_paid) total_sales, sum(t.adult_amount_paid) adult_sales, sum(t.kid_amount_paid) kid_sales, sum(t.adult_seat + t.kid_seat) total_attendance, sum(t.adult_seat) adults_attendance, sum(t.kid_seat) kid_attendance 
+            FROM transaction_fact t, date_dimension d 
+            WHERE t.date_key = d.date_key and extract(month from d.date) = 10 
+            GROUP BY d.weekday 
+            ORDER BY total_sales DESC;""")) #insert query here
         else:
-            cur.execute(('SELECT d.weekday, sum(t.total_amount_paid) total_sales, sum(t.adult_amount_paid) adult_sales, sum(t.kid_amount_paid) kid_sales, sum(t.adult_seat + t.kid_seat) total_attendance, sum(t.adult_seat) adults_attendance, sum(t.kid_seat) kid_attendance FROM transaction_fact t, date_dimension d WHERE t.date_key = d.date_key GROUP BY d.weekday ORDER BY total_sales DESC;')) #insert query here
-        table = cur.fetchall() #fetches query and put into object
+            cur.execute(("""SELECT d.weekday, sum(t.total_amount_paid) total_sales, sum(t.adult_amount_paid) adult_sales, sum(t.kid_amount_paid) kid_sales, sum(t.adult_seat + t.kid_seat) total_attendance, sum(t.adult_seat) adults_attendance, sum(t.kid_seat) kid_attendance 
+            FROM transaction_fact t, date_dimension d 
+            WHERE t.date_key = d.date_key 
+            GROUP BY d.weekday 
+            ORDER BY total_sales DESC;""")) #insert query here
+        #point to existing session and modify
+        session["sales_dow"] = cur.fetchall() #fetches query and put into object
+        session.modified = True
+
         cur.close() #closes query
         conn.close() #closes connection to db
     return render_template('index.html', table=table)
 
 def query_sales_by_film(): #template for some query
-    if request.method == 'GET':
+    if request.method == 'POST':
         Month_choice = request.form['table-choice']
         conn = get_db_connection()
         cur = conn.cursor()
@@ -151,27 +166,21 @@ def query_sales_by_film(): #template for some query
                          sum(t.adult_seat) adults_attendance, sum(t.kid_seat) kid_attendance 
                          FROM transaction_fact t, date_dimension d, film_dimension f WHERE t.date_key = d.date_key 
                          AND t.film_key = f.film_key GROUP BY f.film_name, f.genre ORDER BY total_sales DESC;""")) #insert query here
-        table = cur.fetchall() #fetches query and put into object
+        #point to existing session and modify
+        session["sales_by_film"] = cur.fetchall() #fetches query and put into object
+        session.modified = True
         cur.close() #closes query
         conn.close() #closes connection to db
-    return render_template('index.html', table=table)
+    return render_template('index.html', version=session["db_version"],
+                           sales_dow=session.get("sales_dow"),
+                           sales_by_film=session.get("sales_by_film"),
+                           film_roi=session.get("film_roi"),
+                           promo_roi=session.get("promo_roi"),
+                           pop_promo=session.get("pop_promo"))
 
-def query_film_roi(): #template for some query
-    if request.method == 'GET':
-        conn = get_db_connection()
-        cur = conn.cursor()
-        cur.execute(("""SELECT f.film_name, f.genre, sum(t.total_amount_paid) total_sales, max(f.procurement_cost) cost, (sum(t.total_amount_paid) - max(f.procurement_cost)) profit, ((sum(t.total_amount_paid) - max(f.procurement_cost))/max(f.procurement_cost)) ROI 
-                     FROM transaction_fact t, film_dimension f 
-                     WHERE t.film_key = f.film_key 
-                     GROUP BY f.film_name, f.genre 
-                     ORDER BY roi DESC;""")) #insert query here
-        table = cur.fetchall() #fetches query and put into object
-        cur.close() #closes query
-        conn.close() #closes connection to db
-    return render_template('index.html', table=table)
-
+@app.route('/promo_roi', methods=['POST'])
 def query_promo_roi(): #template for some query
-    if request.method == 'GET':
+    if request.method == 'POST':
         Month_choice = request.form['table-choice']
         conn = get_db_connection()
         cur = conn.cursor()
@@ -193,13 +202,21 @@ def query_promo_roi(): #template for some query
                          WHERE t.date_key = d.date_key AND t.promotion_key = p.promotion_key 
                          GROUP BY p.planner, p.promotion_name 
                          ORDER BY promo_roi DESC;""")) #insert query here
-        table = cur.fetchall() #fetches query and put into object
+        # point to existing session and modify
+        session["promo_roi"] = cur.fetchall()  # fetches query and put into object
+        session.modified = True
         cur.close() #closes query
         conn.close() #closes connection to db
-    return render_template('index.html', table=table)
+    return render_template('index.html', version=session["db_version"],
+                           sales_dow=session.get("sales_dow"),
+                           sales_by_film=session.get("sales_by_film"),
+                           film_roi=session.get("film_roi"),
+                           promo_roi=session.get("promo_roi"),
+                           pop_promo=session.get("pop_promo"))
 
+@app.route('/pop_promo', methods=['POST'])
 def query_pop_promo(): #template for some query
-    if request.method == 'GET':
+    if request.method == 'POST':
         Month_choice = request.form['table-choice']
         conn = get_db_connection()
         cur = conn.cursor()
@@ -220,7 +237,7 @@ def query_pop_promo(): #template for some query
                                 AND t2.member_key = m2.member_key
                                 AND m1.age = m2.age
                                 AND m1.gender = m2.gender
-                                GROUP p2.planner, p2.promotion_name, m2.age, m2.gender)
+                                GROUP BY p2.planner, p2.promotion_name, m2.age, m2.gender)
                             ORDER BY m1.age, m1.gender DESC;""")) #insert query here
         elif Month_choice == 'October':
             cur.execute(("""SELECT m1.age, m1.gender,p1.planner,p1.promotion_name, count(*) total_redemption
@@ -239,7 +256,7 @@ def query_pop_promo(): #template for some query
                                 AND t2.member_key = m2.member_key
                                 AND m1.age = m2.age
                                 AND m1.gender = m2.gender
-                                GROUP p2.planner, p2.promotion_name, m2.age, m2.gender)
+                                GROUP BY p2.planner, p2.promotion_name, m2.age, m2.gender)
                             ORDER BY m1.age, m1.gender DESC;""")) #insert query here
         else:
             cur.execute(("""SELECT m1.age, m1.gender,p1.planner,p1.promotion_name, count(*) total_redemption
@@ -256,12 +273,19 @@ def query_pop_promo(): #template for some query
                                 AND t2.member_key = m2.member_key
                                 AND m1.age = m2.age
                                 AND m1.gender = m2.gender
-                                GROUP p2.planner, p2.promotion_name, m2.age, m2.gender)
+                                GROUP BY p2.planner, p2.promotion_name, m2.age, m2.gender)
                             ORDER BY m1.age, m1.gender DESC;""")) #insert query here
-        table = cur.fetchall() #fetches query and put into object
+        # point to existing session and modify
+        session["pop_promo"] = cur.fetchall()  # fetches query and put into object
+        session.modified = True
         cur.close() #closes query
         conn.close() #closes connection to db
-    return render_template('index.html', table=table)
+    return render_template('index.html', version=session["db_version"],
+                           sales_dow=session.get("sales_dow"),
+                           sales_by_film=session.get("sales_by_film"),
+                           film_roi=session.get("film_roi"),
+                           promo_roi=session.get("promo_roi"),
+                           pop_promo=session.get("pop_promo"))
 
 
 
