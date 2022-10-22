27a168b60bb6f4e4b1f142275ae58dfa7a0b3fa7
il5050 <ivan@brandmotor.com>
2022-10-22 17:12:53 +0800

    update default display to all

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def hello_world():  # put application's code here
     #Sales and Attendance by Day of Week Section Display
     cur.execute("""SELECT d.weekday, sum(t.total_amount_paid) total_sales, sum(t.adult_amount_paid) adult_sales, sum(t.kid_amount_paid) kid_sales, sum(t.adult_seat + t.kid_seat) total_attendance, sum(t.adult_seat) adults_attendance, sum(t.kid_seat) kid_attendance 
                 FROM transaction_fact t, date_dimension d 
-                WHERE t.date_key = d.date_key and extract(month from d.date) = 9 
+                WHERE t.date_key = d.date_key
                 GROUP BY d.weekday ORDER BY total_sales DESC;""")
     session["sales_dow"] = cur.fetchall()
     
@@ -55,7 +55,7 @@ def hello_world():  # put application's code here
     cur.execute("""SELECT f.film_name, f.genre, sum(t.total_amount_paid) total_sales, sum(t.adult_amount_paid) adult_sales, 
                     sum(t.kid_amount_paid) kid_sales, sum(t.adult_seat + t.kid_seat) total_attendance, sum(t.adult_seat) adults_attendance, 
                     sum(t.kid_seat) kid_attendance FROM transaction_fact t, date_dimension d, film_dimension f 
-                    WHERE t.date_key = d.date_key AND extract(month from d.date) = 9 AND t.film_key = f.film_key 
+                    WHERE t.date_key = d.date_key AND t.film_key = f.film_key 
                          GROUP BY f.film_name, f.genre ORDER BY total_sales DESC;""")
     session["sales_by_film"] = cur.fetchall()
     
@@ -70,7 +70,7 @@ def hello_world():  # put application's code here
     #Return on Investment By Film Section Display
     cur.execute("""SELECT p.planner, p.promotion_name, SUM(t.total_amount_paid) total_sales_from_promo, (MAX(p.cost_per_member) * count(*)) promo_costs, (SUM(t.total_amount_paid)-(MAX(p.cost_per_member) * count(*))) profit, (SUM(t.total_amount_paid)-(MAX(p.cost_per_member) * count(*)))/(MAX(p.cost_per_member) * count(*)) promo_roi 
                          FROM transaction_fact t, date_dimension d, promotion_dimension p 
-                         WHERE t.date_key = d.date_key AND extract(month from d.date) = 9 AND t.promotion_key = p.promotion_key 
+                         WHERE t.date_key = d.date_key AND t.promotion_key = p.promotion_key 
                          GROUP BY p.planner, p.promotion_name 
                          ORDER BY promo_roi DESC;""")
     session["promo_roi"] = cur.fetchall()
@@ -79,7 +79,6 @@ def hello_world():  # put application's code here
     cur.execute("""SELECT m1.age, m1.gender,p1.planner,p1.promotion_name, count(*) total_redemption
                             FROM transaction_fact t1, date_dimension d1, promotion_dimension p1, member_dimension m1
                             WHERE t1.date_key = d1.date_key 
-                            AND extract(month from d1.date) = 9
                             AND t1.promotion_key = p1.promotion_key
                             AND t1.member_key = m1.member_key
                             GROUP BY p1.planner, p1.promotion_name, m1.age, m1.gender
@@ -87,7 +86,6 @@ def hello_world():  # put application's code here
                                 SELECT count(*) 
                                 FROM transaction_fact t2, date_dimension d2, promotion_dimension p2, member_dimension m2
                                 WHERE t2.date_key = d2.date_key 
-                                AND extract(month from d2.date) = 9
                                 AND t2.promotion_key = p2.promotion_key
                                 AND t2.member_key = m2.member_key
                                 AND m1.age = m2.age
